b19a2e044cebec405a01d5ed627dcf6d92c5aa16
anyanelson <anelson@newroads.org>
2018-03-05 10:42:51 -0800

    ijo

--- aboutme2.js
+++ aboutme2.js
@@ -4,7 +4,7 @@ var mycanvas = document.getElementById('mycanvas');
     var w = 350;
     var h = 350;
     var score = 0;
-    var snake;
+    var snake;n
     var snakeSize = 10;
     var food; 
 
